62740cbb1a466d56e4009278b26d67c9c8077229
coredumpninja <gordonmcone@gmail.com>
2015-06-01 14:58:33 -0400

    Correct compile issue for UNO.
    Moved SPI defines from SAM3X8E to AVR.  Also found ramp example to work under DUE (after changing order of includes to include SPI first).

--- analogShield.h
+++ analogShield.h
@@ -36,6 +36,8 @@
 /*	04/22/2014(WilliamE): Created										*/
 /*  05/27/2014(MarshallW): Modified for readability and content         */
 /*  02/16/2015(MarshallW): ChipKIT Efficiency update!                   */
+/*  06/01/2015(GordonC):  Fixed issue for Uno compile.                  */
+/*                        Tested ramp example with DUE                  */
 /*																		*/
 /*  Todo:                                                               */
 /*    - Framework for DUE added but not tested                          */
@@ -48,45 +50,44 @@
 #ifndef _analogShield_h_
 #define _analogShield_h_
 
-    #if defined(__PIC32MX__)
-        #include <WProgram.h>
-        #include <inttypes.h>
-        #include <SPI.h>
-
+	#if defined(__PIC32MX__)
+		#include <WProgram.h>
+		#include <inttypes.h>
+		#include <SPI.h>
+	
 	#elif defined(__AVR__)
 		#include <stdio.h>
 		#include <Arduino.h>
 		#include <avr/pgmspace.h>
 		
+		//define some SPI configs
+		#define SPI_CLOCK_DIV4 0x00
+		#define SPI_CLOCK_DIV16 0x01
+		#define SPI_CLOCK_DIV64 0x02
+		#define SPI_CLOCK_DIV128 0x03
+		#define SPI_CLOCK_DIV2 0x04
+		#define SPI_CLOCK_DIV8 0x05
+		#define SPI_CLOCK_DIV32 0x06
+		//#define SPI_CLOCK_DIV64 0x07
+
+		#define SPI_MODE0 0x00
+		#define SPI_MODE1 0x04
+		#define SPI_MODE2 0x08
+		#define SPI_MODE3 0x0C
+
+		#define SPI_MODE_MASK 0x0C  // CPOL = bit 3, CPHA = bit 2 on SPCR
+		#define SPI_CLOCK_MASK 0x03  // SPR1 = bit 1, SPR0 = bit 0 on SPCR
+		#define SPI_2XCLOCK_MASK 0x01  // SPI2X = bit 0 on SPSR
+
 	#elif defined (__SAM3X8E__)
 		#include <stdio.h>
 		#include <Arduino.h>
 		#include <avr/pgmspace.h>
-        #include <SPI.h>
-
-        //define some SPI configs
-        #define SPI_CLOCK_DIV4 0x00
-        #define SPI_CLOCK_DIV16 0x01
-        #define SPI_CLOCK_DIV64 0x02
-        #define SPI_CLOCK_DIV128 0x03
-        #define SPI_CLOCK_DIV2 0x04
-        #define SPI_CLOCK_DIV8 0x05
-        #define SPI_CLOCK_DIV32 0x06
-        //#define SPI_CLOCK_DIV64 0x07
-
-        #define SPI_MODE0 0x00
-        #define SPI_MODE1 0x04
-        #define SPI_MODE2 0x08
-        #define SPI_MODE3 0x0C
-
-        #define SPI_MODE_MASK 0x0C  // CPOL = bit 3, CPHA = bit 2 on SPCR
-        #define SPI_CLOCK_MASK 0x03  // SPR1 = bit 1, SPR0 = bit 0 on SPCR
-        #define SPI_2XCLOCK_MASK 0x01  // SPI2X = bit 0 on SPSR
-
+		#include <SPI.h>
 	#else //throw error
-        #error "Incorrect processor type selected"
+		#error "Incorrect processor type selected"
 	#endif
-		
+
 	#include <stdint.h>
 	
 	//pins
@@ -107,7 +108,7 @@
 		
 	public:
 	
-    #if defined(__PIC32MX__)
+    	#if defined(__PIC32MX__)
 		volatile uint32_t *ADCCSSet;
 		volatile uint32_t *ADCCSClr;
 		uint32_t ADCCSPinMask;
